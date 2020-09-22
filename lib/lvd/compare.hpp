cd287914d83eeaa7b54537e28238b33818fa67f9
Victor Dods <victor.dods@gmail.com>
2020-09-22 01:40:53 -0700

    Moved the dir structure around to improve organization.

---
+++ lib/lvd/compare.hpp
@@ -0,0 +1,25 @@
+// 2020.09.12 - Victor Dods
+
+#pragma once
+
+namespace lvd {
+
+// Runtime complexity O(size); lexicographical ordering (not shortlex order).
+template <typename Iterator_>
+int compare_lexicographical (Range_t<Iterator_> const &lhs, Range_t<Iterator_> const &rhs) {
+    for (Iterator_ lhs_it = lhs.begin(), rhs_it = rhs.begin(); lhs_it != lhs.end() && rhs_it != rhs.end(); ++lhs_it, ++rhs_it) {
+
+    }
+
+    for (size_t i = 0; i < lhs.size() && i < rhs.size(); ++i) {
+        auto c = compare_data(lhs[i], rhs[i]);
+        if (c != 0)
+            return c;
+    }
+    // If we got this far, then they match on their common length, so the shorter one is "less".
+    return lhs.size() < rhs.size() ? -1 : (lhs.size() == rhs.size() ? 0 : 1);
+}
+
+// TODO: Implement compare_shortlex
+
+} // end namespace lvd
