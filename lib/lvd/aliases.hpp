1be97f242d5adf6bc30cecc0f6dd0b65c57cf9e4
Victor Dods <victor.dods@gmail.com>
2020-11-12 10:54:37 -0800

    Added sp and nnsp pointer aliases.

--- lib/lvd/aliases.hpp
+++ lib/lvd/aliases.hpp
@@ -18,8 +18,14 @@ using nn = gsl::not_null<T_>;
 template <typename T_>
 using nnp = gsl::not_null<T_*>;
 
+template <typename T_>
+using sp = std::shared_ptr<T_>;
+
 template <typename T_, typename Deleter_ = std::default_delete<T_>>
 using up = std::unique_ptr<T_,Deleter_>;
+
+template <typename T_>
+using nnsp = gsl::not_null<std::shared_ptr<T_>>;
 
 template <typename T_, typename Deleter_ = std::default_delete<T_>>
 using nnup = gsl::not_null<std::unique_ptr<T_,Deleter_>>;
@@ -27,6 +33,14 @@ using nnup = gsl::not_null<std::unique_ptr<T_,Deleter_>>;
 //
 // Analogs to std::make_* functions
 //
+
+// A convenience function for creating a gsl::not_null<std::shared_ptr<T_>>.
+// This is equivalent to lvd::make_not_null_shared_ptr.
+template <typename T_, typename... Args_>
+nnsp<T_> make_nnsp (Args_&&... args)
+{
+    return nnsp<T_>(std::make_shared<T_>(std::forward<Args_>(args)...));
+}
 
 // A convenience function for creating a gsl::not_null<std::unique_ptr<T_>>.
 // Note that there's currently no way to specify a custom deleter.  This is
