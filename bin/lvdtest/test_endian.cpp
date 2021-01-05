78768400d0d2bacda50fe488dee8f871c29ad4f6
Victor Dods <victor.dods@gmail.com>
2021-01-05 03:34:36 -0800

    Added endian.hpp, which provides a function to determine machine's byte order, and provides byte-order swapping routines.

---
+++ bin/lvdtest/test_endian.cpp
@@ -0,0 +1,67 @@
+// 2021.01.04 - Copyright Victor Dods - Licensed under Apache 2.0
+
+#include "lvd/endian.hpp"
+#include "lvd/random.hpp"
+#include "lvd/req.hpp"
+#include "lvd/test.hpp"
+#include <random>
+
+namespace lvd {
+
+template <typename T_>
+bool is_endian_symmetric (T_ const &x) {
+    static_assert(sizeof(T_) == 1 || sizeof(T_) == 2 || sizeof(T_) == 4 || sizeof(T_) == 8);
+    auto bytes = reinterpret_cast<std::byte const *>(&x);
+    if constexpr (sizeof(T_) == 1) {
+        return true;
+    } else if constexpr (sizeof(T_) == 2) {
+        return bytes[0] == bytes[1];
+    } else if constexpr (sizeof(T_) == 4) {
+        return bytes[0] == bytes[3] && bytes[1] == bytes[2];
+    } else if constexpr (sizeof(T_) == 8) {
+        return bytes[0] == bytes[7] && bytes[1] == bytes[6] && bytes[2] == bytes[5] && bytes[3] == bytes[4];
+    }
+}
+
+// 322 must be >= 321 (which is random), since endian.hpp depends on that.
+LVD_TEST_BEGIN(322__endian)
+    test_log << Log::trc() << LVD_REFLECT(machine_is_big_endian()) << '\n';
+    test_log << Log::trc() << LVD_REFLECT(machine_is_little_endian()) << '\n';
+    test_log << Log::trc() << LVD_REFLECT(machine_endianness()) << '\n';
+
+    auto rng = std::mt19937{42};
+    for (uint32_t i = 0; i < 0x10000; ++i) {
+        auto x1 = make_random<uint8_t>(rng);
+        auto x2 = make_random<uint16_t>(rng);
+        auto x4 = make_random<uint32_t>(rng);
+        auto x8 = make_random<uint64_t>(rng);
+
+//         test_log << Log::trc() << LVD_REFLECT(x1) << ", " << LVD_REFLECT(swapped_byte_order_of(x1)) << '\n';
+//         test_log << Log::trc() << LVD_REFLECT(x2) << ", " << LVD_REFLECT(swapped_byte_order_of(x2)) << '\n';
+//         test_log << Log::trc() << LVD_REFLECT(x4) << ", " << LVD_REFLECT(swapped_byte_order_of(x4)) << '\n';
+//         test_log << Log::trc() << LVD_REFLECT(x8) << ", " << LVD_REFLECT(swapped_byte_order_of(x8)) << '\n';
+
+        if (is_endian_symmetric(x1))
+            LVD_TEST_REQ_EQ(swapped_byte_order_of(x1), x1);
+        else
+            LVD_TEST_REQ_NEQ(swapped_byte_order_of(x1), x1);
+
+        if (is_endian_symmetric(x2))
+            LVD_TEST_REQ_EQ(swapped_byte_order_of(x2), x2);
+        else
+            LVD_TEST_REQ_NEQ(swapped_byte_order_of(x2), x2);
+
+        if (is_endian_symmetric(x4))
+            LVD_TEST_REQ_EQ(swapped_byte_order_of(x4), x4);
+        else
+            LVD_TEST_REQ_NEQ(swapped_byte_order_of(x4), x4);
+
+        if (is_endian_symmetric(x8))
+            LVD_TEST_REQ_EQ(swapped_byte_order_of(x8), x8);
+        else
+            LVD_TEST_REQ_NEQ(swapped_byte_order_of(x8), x8);
+    }
+LVD_TEST_END
+
+} // end namespace lvd
+
---
+++ lib/lvd/endian.hpp
@@ -0,0 +1,84 @@
+// 2021.01.04 - Copyright Victor Dods - Licensed under Apache 2.0
+
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+namespace lvd {
+
+enum class Endianness : uint8_t {
+    BIG = 0,
+    LITTLE,
+
+    __LOWEST__ = BIG,
+    __HIGHEST__ = LITTLE,
+};
+
+inline std::ostream &operator << (std::ostream &out, Endianness e) {
+    auto constexpr COUNT = size_t(Endianness::__HIGHEST__) - size_t(Endianness::__LOWEST__) + 1;
+    static std::array<std::string,COUNT> const TABLE{
+        "BIG",
+        "LITTLE",
+    };
+    return out << TABLE.at(uint32_t(e));
+}
+
+// Reference: http://esr.ibiblio.org/?p=5095
+inline bool constexpr machine_is_big_endian () {
+    return (*(uint16_t *)"\0\xff" < 0x100);
+}
+
+inline bool constexpr machine_is_little_endian () {
+    return !machine_is_big_endian();
+}
+
+inline Endianness constexpr machine_endianness () {
+    if (machine_is_big_endian())
+        return Endianness::BIG;
+    else
+        return Endianness::LITTLE;
+}
+
+template <typename T_, typename = std::enable_if_t<std::is_trivially_copyable_v<T_>>>
+void swap_byte_order_of (T_ &value) {
+    static_assert(sizeof(T_) == 1 || sizeof(T_) == 2 || sizeof(T_) == 4 || sizeof(T_) == 8, "can only swap_byte_order_of a type of size 1, 2, 4, or 8.");
+
+    auto bytes = reinterpret_cast<std::byte *>(&value);
+    if constexpr (sizeof(T_) == 1) {
+        // Nothing to do.
+    } else if constexpr (sizeof(T_) == 2) {
+        std::swap(bytes[0], bytes[1]);
+    } else if constexpr (sizeof(T_) == 4) {
+        std::swap(bytes[0], bytes[3]);
+        std::swap(bytes[1], bytes[2]);
+    } else if constexpr (sizeof(T_) == 8) {
+        std::swap(bytes[0], bytes[7]);
+        std::swap(bytes[1], bytes[6]);
+        std::swap(bytes[2], bytes[5]);
+        std::swap(bytes[3], bytes[4]);
+    }
+}
+
+template <typename T_, typename = std::enable_if_t<std::is_trivially_copyable_v<T_>>>
+T_ swapped_byte_order_of (T_ value) {
+    swap_byte_order_of(value);
+    return value;
+}
+
+template <typename T_, typename = std::enable_if_t<std::is_trivially_copyable_v<T_>>>
+void endian_change (Endianness from, Endianness to, T_ &value) {
+    if (from != to)
+        swap_byte_order_of(value);
+}
+
+template <typename T_, typename = std::enable_if_t<std::is_trivially_copyable_v<T_>>>
+T_ endian_changed (Endianness from, Endianness to, T_ value) {
+    endian_change(from, to, value);
+    return value;
+}
+
+} // end namespace lvd
